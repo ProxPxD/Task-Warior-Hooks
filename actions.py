2b769bf60766282faa8a2a7be1937d3e65a03491
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-11-29 12:46:49 +0100

    tag

--- actions.py
+++ actions.py
@@ -52,7 +52,7 @@ class ReverseDependency(Action):
 class Autotag(Action):
     same_tagging = [
         'buy', 'learn', 'move', 'design', 'server', 'fix',
-        'game', 'taskwarrior', 'ai',
+        'game', 'taskwarrior', 'ai', 'todo',
     ]
     pattern_to_tags = {
         '(vault|bit)warden': ['vaultwarden', 'password', 'self.hosting', 'security'],
